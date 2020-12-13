35bfc384e8ae4d5f3759f096023fd78f13ac305c
Crisis21 <73611327+Crisis21@users.noreply.github.com>
2020-12-13 12:22:14 -0600

    Delete addition.sh

--- math_scripts/math_scripts/addition.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "What is the first number you would like to add?"
-read num1
-echo "What is the second number you would like to add?"
-read num2
-echo "your answer is:"
-expr $num1 + $num2
