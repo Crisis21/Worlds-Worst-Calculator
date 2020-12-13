7313a86670c9308026bef8d0b6f438498149b190
Crisis21 <73611327+Crisis21@users.noreply.github.com>
2020-12-13 12:22:30 -0600

    Delete multiplication.sh

--- math_scripts/math_scripts/multiplication.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "What is the first number you would like to multiply?"
-read num1
-echo "What is the second number you would like to multiply?"
-read num2
-echo "your answer is:"
-expr $num1 \* $num2
